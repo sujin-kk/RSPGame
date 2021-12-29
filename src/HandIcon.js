af321082743faf29209dd74df6810cb54cec1f78
sujin-kk <sjoo0002@gmail.com>
2021-12-30 00:10:47 +0900

    first commit

---
+++ src/HandIcon.js
@@ -0,0 +1,18 @@
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
+import './HandIcon.css';
+
+const IMAGES = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+
+
+function HandIcon({ value, className=''}) {
+  const src = IMAGES[value];
+  return <img className="Hand-icon" src={src} alt={value} />;
+}
+
+export default HandIcon;
